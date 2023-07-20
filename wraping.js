e955007367d67268e73cc72152657c2e3d645021
Sven Steinert <svensteinert96@gmail.com>
2023-07-20 09:37:01 +0200

    add orcid

--- wraping.js
+++ wraping.js
@@ -3,3 +3,18 @@ function togglebg() {
   var bgimg = document.getElementById('bgimg');
   bgimg.classList.toggle('fade');
 }
+
+// Check if the device is a mobile phone
+function isMobileDevice() {
+  return /Mobi/i.test(navigator.userAgent);
+}
+
+// Function to disable scrolling on mobile devices
+function disableScrollOnMobile() {
+  if (isMobileDevice()) {
+    document.body.style.overflow = "hidden";
+  }
+}
+
+// Call the function to disable scrolling on mobile devices
+disableScrollOnMobile();
