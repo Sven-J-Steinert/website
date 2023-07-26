d58170ce74042a00237a309e411d0a964809aa46
Sven Steinert <svensteinert96@gmail.com>
2023-07-26 13:07:53 +0200

    solar complexity

--- GPT-3.js
+++ GPT-3.js
@@ -3,8 +3,11 @@ function launchAni() {
   /* This code was in its core created by GPT-3 javascript-sandbox with the following command */
 
   /* objects that attract each other by gravity */
+
+  /* further modifications have been build up on that */
+
   var G = 1;
-  var force_max = 10;
+  var force_max = 100;
   var objects = [];
   var canvas = document.createElement('canvas');
   canvas.width = window.innerWidth;
@@ -20,13 +23,63 @@ function launchAni() {
     this.vx = vx;
     this.vy = vy;
     this.mass = mass;
+    this.shouldMerge = false;
   }
 
   function addObject(x, y, vx, vy, mass) {
     objects.push(new Object(x, y, vx, vy, mass));
   }
 
+  function mergeObjects(obj1, obj2) {
+    // Combine masses and momenta to create a new entity
+    var totalMass = obj1.mass + obj2.mass;
+    var mergedVx = (obj1.mass * obj1.vx + obj2.mass * obj2.vx) / totalMass;
+    var mergedVy = (obj1.mass * obj1.vy + obj2.mass * obj2.vy) / totalMass;
+  
+    // Calculate the weighted average position based on masses
+    var mergedX = (obj1.x * obj1.mass + obj2.x * obj2.mass) / totalMass;
+    var mergedY = (obj1.y * obj1.mass + obj2.y * obj2.mass) / totalMass;
+  
+    // Create the new merged object
+    return new Object(mergedX, mergedY, mergedVx, mergedVy, totalMass);
+  }
+  
+
+  function checkCollisions() {
+    for (var i = 0; i < objects.length; i++) {
+      var obj1 = objects[i];
+      for (var j = i + 1; j < objects.length; j++) {
+        var obj2 = objects[j];
+        var dx = obj1.x - obj2.x;
+        var dy = obj1.y - obj2.y;
+        var dist = Math.sqrt(dx * dx + dy * dy);
+        var minDist = Math.sqrt(obj1.mass) + Math.sqrt(obj2.mass);
+  
+        if (dist < minDist && !obj1.shouldMerge && !obj2.shouldMerge) {
+          obj1.shouldMerge = true;
+          obj2.shouldMerge = true;
+          obj1.collisionPartner = obj2; // Store the partner object for merging
+          obj2.collisionPartner = obj1; // Store the partner object for merging
+        }
+      }
+    }
+  }
+
   function update() {
+    checkCollisions();
+
+    // Merge collided objects
+    for (var i = objects.length - 1; i >= 0; i--) {
+      var obj = objects[i];
+      if (obj.shouldMerge) {
+        var mergedObj = mergeObjects(obj, obj.collisionPartner);
+        objects.splice(i, 1); // Remove the first object from the array
+        objects.splice(objects.indexOf(obj.collisionPartner), 1); // Remove the second object from the array
+        objects.push(mergedObj); // Add the merged object to the array
+      }
+    }
+
+    // Update velocities and positions for non-merged objects
     for (var i = 0; i < objects.length; i++) {
       var obj1 = objects[i];
       for (var j = i + 1; j < objects.length; j++) {
@@ -35,7 +88,7 @@ function launchAni() {
         var dy = obj1.y - obj2.y;
         var dist = Math.sqrt(dx * dx + dy * dy);
         var force = G * obj1.mass * obj2.mass / (dist * dist);
-        if (force > force_max ) { force = 4;}
+        if (force > force_max) { force = 4; }
         var ax = force * dx / dist;
         var ay = force * dy / dist;
         obj1.vx -= ax / obj1.mass;
@@ -44,11 +97,13 @@ function launchAni() {
         obj2.vy += ay / obj2.mass;
       }
     }
+
+    // Update positions for non-merged objects
     for (var i = 0; i < objects.length; i++) {
       var obj = objects[i];
       obj.x += obj.vx;
       obj.y += obj.vy;
-    }
+      }
   }
 
   function draw() {
@@ -67,18 +122,41 @@ function launchAni() {
     requestAnimationFrame(loop);
   }
 
+  // Function to generate a random number within a range
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
 
-  addObject(cent_w , cent_h, 0, 0, 1000);
-  addObject(cent_w + 480, cent_h + 100, 0, -1, 50);
+  // Function to generate a random object
+  function generateRandomObject(min_mass,max_mass) {
+    var x = randomInRange(0, canvas.width);
+    var y = randomInRange(0, canvas.height);
+    var vx = randomInRange(-1, 1);
+    var vy = randomInRange(-1, 1);
+    var mass = randomInRange(min_mass, max_mass);
+    addObject(x, y, vx, vy, m_factor*mass);
+  }
 
-  addObject(cent_w + 350, cent_h - 50, 0, -2,5);
-  addObject(cent_w + 360, cent_h - 50, 0, -1.2,5);
+  // Determine PixelAera for scaling
+  var A = canvas.width * canvas.height;
+  if (A < 600000) {A = 600000;} // minimum scale
+  var A_ref = 3345920; // 4k design reference 2560*1307
+  var m_factor = A / A_ref;
 
-  addObject(cent_w -100, cent_h - 100, -2, 2, 30);
-  addObject(cent_w, cent_h - 100, -3, 0, 10);
+  // Generate dust
+  var numRandomObjects = 1000;
+  for (var i = 0; i < numRandomObjects; i++) {
+    generateRandomObject(0.5,1);
+  }
 
-  addObject(cent_w -320, cent_h, 0, 1.5,15);
-  addObject(cent_w -330, cent_h, 0, 2.8,2);
+  // Generate medium
+  var numRandomObjects = 20; 
+  for (var i = 0; i < numRandomObjects; i++) {
+    generateRandomObject(5,10);
+  }
+
+  // Generate protosun
+  addObject(cent_w , cent_h, 0, 0, m_factor*100);
 
   loop();
 
