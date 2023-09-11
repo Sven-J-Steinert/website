76b6e1daecb0b04ed0a7c7e70d05f999a5e73df8
Sven Steinert <svensteinert96@gmail.com>
2023-09-12 01:09:47 +0200

    backup

--- GPT-3.js
+++ GPT-3.js
@@ -1,11 +1,4 @@
 function launchAni() {
-
-  /* This code was in its core created by GPT-3 javascript-sandbox with the following command */
-
-  /* objects that attract each other by gravity */
-
-  /* further modifications have been build up on that */
-
   var G = 1;
   var force_max = 100;
   var objects = [];
@@ -37,26 +30,27 @@ function launchAni() {
     var totalMass = obj1.mass + obj2.mass;
     var mergedVx = (obj1.mass * obj1.vx + obj2.mass * obj2.vx) / totalMass;
     var mergedVy = (obj1.mass * obj1.vy + obj2.mass * obj2.vy) / totalMass;
-  
+
     // Calculate the weighted average position based on masses
     var mergedX = (obj1.x * obj1.mass + obj2.x * obj2.mass) / totalMass;
     var mergedY = (obj1.y * obj1.mass + obj2.y * obj2.mass) / totalMass;
-  
+
     // Create the new merged object
     return new Object(mergedX, mergedY, mergedVx, mergedVy, totalMass);
   }
-  
 
   function checkCollisions() {
     for (var i = 0; i < objects.length; i++) {
       var obj1 = objects[i];
+      var mass1Sqrt = Math.sqrt(obj1.mass);
+
       for (var j = i + 1; j < objects.length; j++) {
         var obj2 = objects[j];
         var dx = obj1.x - obj2.x;
         var dy = obj1.y - obj2.y;
         var dist = Math.sqrt(dx * dx + dy * dy);
-        var minDist = Math.sqrt(obj1.mass) + Math.sqrt(obj2.mass);
-  
+        var minDist = mass1Sqrt + Math.sqrt(obj2.mass);
+
         if (dist < minDist && !obj1.shouldMerge && !obj2.shouldMerge) {
           obj1.shouldMerge = true;
           obj2.shouldMerge = true;
@@ -90,7 +84,9 @@ function launchAni() {
         var dy = obj1.y - obj2.y;
         var dist = Math.sqrt(dx * dx + dy * dy);
         var force = G * obj1.mass * obj2.mass / (dist * dist);
-        if (force > force_max) { force = 4; }
+        if (force > force_max) {
+          force = 4;
+        }
         var ax = force * dx / dist;
         var ay = force * dy / dist;
         obj1.vx -= ax / obj1.mass;
@@ -105,32 +101,30 @@ function launchAni() {
       var obj = objects[i];
       obj.x += obj.vx;
       obj.y += obj.vy;
-      }
-
-    
+    }
   }
 
-
-
-
-
-function draw() {
-  // Draw objects
-  ctx.fillStyle = obj_color;
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (var i = 0; i < objects.length; i++) {
-    var obj = objects[i];
-    ctx.beginPath();
-    ctx.arc(obj.x, obj.y, Math.sqrt(obj.mass), 0, 2 * Math.PI);
-    ctx.fill();
+  function draw() {
+    // Draw objects
+    ctx.fillStyle = obj_color;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (var i = 0; i < objects.length; i++) {
+      var obj = objects[i];
+      ctx.beginPath();
+      ctx.arc(obj.x, obj.y, Math.sqrt(obj.mass), 0, 2 * Math.PI);
+      ctx.fill();
+    }
   }
 
-}
-
-
-  function loop() {
-    update();
-    draw();
+  var lastTime = 0;
+  var frameRate = 1000 / 60; // 60 FPS
+  function loop(timestamp) {
+    var deltaTime = timestamp - lastTime;
+    if (deltaTime >= frameRate) {
+      lastTime = timestamp;
+      update();
+      draw();
+    }
     requestAnimationFrame(loop);
   }
 
@@ -140,36 +134,30 @@ function draw() {
   }
 
   // Function to generate a random object
-  function generateRandomObject(min_mass,max_mass) {
+  function generateRandomObject(min_mass, max_mass) {
     var x = randomInRange(0, canvas.width);
     var y = randomInRange(0, canvas.height);
     var vx = randomInRange(-1, 1);
     var vy = randomInRange(-1, 1);
     var mass = randomInRange(min_mass, max_mass);
-    addObject(x, y, vx, vy, m_factor*mass);
+    addObject(x, y, vx, vy, m_factor * mass);
   }
 
   // Determine PixelAera for scaling
   var A = canvas.width * canvas.height;
-  if (A < 600000) {A = 600000;} // minimum scale
+  if (A < 600000) {
+    A = 600000;
+  } // minimum scale
   var A_ref = 3345920; // 4k design reference 2560*1307
   var m_factor = A / A_ref;
 
   // Generate dust
-  var numRandomObjects = 2000;
+  var numRandomObjects = 1000;
   for (var i = 0; i < numRandomObjects; i++) {
-    generateRandomObject(0.1,0.5);
+    generateRandomObject(0.2, 1);
   }
 
-  // Generate medium
-  //var numRandomObjects = 0; 
-  //for (var i = 0; i < numRandomObjects; i++) {
-  //  generateRandomObject(5,10);
-  //}
-
-  // Generate protosun
-  //addObject(cent_w , cent_h, 0, 0, m_factor*10);
-
-  loop();
-
+  requestAnimationFrame(loop);
 }
+
+launchAni();
